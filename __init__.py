2e539912ec0753f1f0c25269adfeb08fb347f6d5
brs-Nathan <nathanberros@gmail.com>
2025-03-21 10:51:19 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,10 +1,6 @@
 from cryptography.fernet import Fernet
+from flask import Flask
 import base64
-from flask import Flask, render_template_string, render_template, jsonify
-from flask import render_template
-from flask import json
-from urllib.request import urlopen
-import sqlite3
 
 app = Flask(__name__)
 
@@ -16,7 +12,7 @@ def generer_fernet(cle_utilisateur):
     try:
         cle_bytes = base64.urlsafe_b64decode(cle_utilisateur)
         return Fernet(cle_bytes)
-    except Exception as e:
+    except Exception:
         return None  # Retourne None si la clé est invalide
 
 @app.route('/encrypt/<string:cle>/<string:valeur>')
@@ -42,3 +38,5 @@ def decryptage(cle, token):
     except Exception as e:
         return f"Erreur lors du déchiffrement : {str(e)}"
 
+if __name__ == "__main__":
+    app.run(debug=True)
